1cd242406b4d29fb2e40006b6339f0bd459ad3d3
1Illuminati <heu2647@gmail.com>
2023-07-28 21:56:15 +0900

    fuck

--- src/main/java/org/red/library/game/setting/Setting.java
+++ src/main/java/org/red/library/game/setting/Setting.java
@@ -1,20 +1,70 @@
 package org.red.library.game.setting;
 
 import org.bukkit.NamespacedKey;
+import org.bukkit.event.inventory.ClickType;
 
-public class Setting<T> {
-    private final NamespacedKey key;
-    private final T defaultValue;
-    public Setting(NamespacedKey key, T defaultValue) {
-        this.key = key;
-        this.defaultValue = defaultValue;
+public abstract class Setting {
+    private final Properties[] properties;
+    private final Object[] values;
+
+    public Setting() {
+        this.properties = getProperties();
+        this.values = new Object[properties.length];
+
+        for (int i = 0; i < properties.length; i++) {
+            values[i] = properties[i].getDefaultValue();
+        }
     }
 
-    public NamespacedKey getKey() {
-        return key;
+    public abstract Properties[] getProperties();
+
+    public abstract SettingInventory getInventory();
+
+    public <T> T getValue(Properties<T> properties) {
+        for (int i = 0; i < this.properties.length; i++) {
+            if (this.properties[i].getKey().equals(properties.getKey())) {
+                return (T) values[i];
+            }
+        }
+
+        throw new IllegalArgumentException("No such properties");
     }
 
-    public T getDefaultValue() {
-        return defaultValue;
+    public <T> void setValue(Properties<T> properties, T value) {
+        for (int i = 0; i < this.properties.length; i++) {
+            if (this.properties[i].getKey().equals(properties.getKey())) {
+                values[i] = value;
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException("No such properties");
+    }
+
+    public void resetSetting() {
+        for (int i = 0; i < properties.length; i++) {
+            values[i] = properties[i].getDefaultValue();
+        }
+    }
+
+    public static class Properties<T> {
+        private final NamespacedKey key;
+        private final T defaultValue;
+        protected Properties(NamespacedKey key, T defaultValue) {
+            this.key = key;
+            this.defaultValue = defaultValue;
+        }
+
+        public NamespacedKey getKey() {
+            return key;
+        }
+
+        public T getDefaultValue() {
+            return defaultValue;
+        }
+
+        public Class<T> getClassType() {
+            return (Class<T>) defaultValue.getClass();
+        }
     }
 }
--- src/main/java/org/red/library/game/setting/SettingInventory.java
+++ src/main/java/org/red/library/game/setting/SettingInventory.java
@@ -1,11 +1,59 @@
 package org.red.library.game.setting;
 
+import org.bukkit.Material;
+import org.bukkit.event.inventory.ClickType;
 import org.red.library.inventory.CustomGui;
+import org.red.library.item.ItemBuilder;
 
-public class SettingInventory extends CustomGui {
-    protected SettingInventory(String title, int size) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public abstract class SettingInventory extends CustomGui {
+    private final Setting setting;
+    public SettingInventory(String title, int size, Setting setting) {
         super(title, size);
+        this.setting = setting;
     }
 
+    public SettingInventory(Setting setting) {
+        this("게임 설정", 27, setting);
+    }
 
+    public Setting getSetting() {
+        return this.setting;
+    }
+
+    protected void setResetButton(int slot, Material material) {
+        this.setItem(slot, new ItemBuilder(material).setDisplayName("§f초기화").setLore("§f모든 설정을 초기화합니다.").build());
+    }
+
+    protected void setBooleanPropertiesButton(int slot, Material material, Setting.Properties<Boolean> properties) {
+        this.setItem(slot, new ItemBuilder(material).setDisplayName(properties.getKey().getKey()).
+                setLore(setting.getValue(properties) ? "§aTrue" : "§cFalse").build());
+
+        this.setButton(slot, event -> {
+            event.setCancelled(true);
+            setting.setValue(properties, !setting.getValue(properties));
+        });
+    }
+
+    protected <T extends Number> void setIntegerPropertiesButton(int slot, Material material, Setting.Properties<T> properties, Map<ClickType, T> clickTypeValue, T max, T min) {
+        List<String> lore = new ArrayList<>();
+        lore.add("§f현재 값: " + setting.getValue(properties));
+        for (ClickType type : clickTypeValue.keySet()) {
+            lore.add("§f" + type.name() + " +" + clickTypeValue.get(type));
+        }
+
+        this.setItem(slot, new ItemBuilder(material).setDisplayName(properties.getKey().getKey()).setLore(lore).build());
+
+        this.setButton(slot, event -> {
+            event.setCancelled(true);
+
+            if (properties.getClassType() == Integer.class) {
+                Number value = setting.getValue(properties).intValue() + clickTypeValue.get(event.getClick()).intValue();
+                this.setting.setValue(properties, (T) value);
+            }
+        });
+    }
 }
